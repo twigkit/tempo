13274ace49855977c024768e062fc0b4d1b43fc8
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2011-02-16 17:43:54 +0000

    problem removing from table in ie

--- src/simplate.js
+++ src/simplate.js
@@ -184,16 +184,13 @@ var SIMPLATE = (function(simplate) {
 		},
 
 		clearContainer : function (el) {
-			if (el != null) {
-				el.innerHTML = '';
+			if (el != null && el.childNodes != undefined) {
+				for (var i = el.childNodes.length; i >= 0; i--) {
+					if (el.childNodes[i] != null && el.childNodes[i].getAttribute != undefined && el.childNodes[i].getAttribute('data-template') != null) {
+						el.childNodes[i].parentNode.removeChild(el.childNodes[i]);
+					}
+				}
 			}
-//			if (el != null && el.childNodes != undefined) {
-//				for (var i = el.childNodes.length; i >= 0; i--) {
-//					if (el.childNodes[i] != null && el.childNodes[i].getAttribute != undefined && el.childNodes[i].getAttribute('data-template') != null) {
-//						el.childNodes[i].parentNode.removeChild(el.childNodes[i]);
-//					}
-//				}
-//			}
 		},
 
 		isNested : function(el) {
