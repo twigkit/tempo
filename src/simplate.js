1e91964f971571bb2fe37c00bade40b5397bace0
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2011-02-16 19:18:44 +0000

    fixed inner templates issue in IE

--- src/simplate.js
+++ src/simplate.js
@@ -1,214 +1,207 @@
 var SIMPLATE = (function(simplate) {
 
-	/*!
+    /*!
 	 * Prepare a container for rendering, gathering templates and clearing afterwards.
 	 */
-	simplate.prepare = function(container) {
-		if (typeof container === 'string') {
-			container = document.getElementById(container);
-		}
+    simplate.prepare = function(container) {
+        if (typeof container === 'string') {
+            container = document.getElementById(container);
+        }
 
-		var templates = new Templates();
-		templates.parse(container);
+        var templates = new Templates();
+        templates.parse(container);
 
-		return new Renderer(templates, container);
-	}
-	simplate.prepare.displayName = 'prepare'
+        return new Renderer(templates, container);
+    }
+    simplate.prepare.displayName = 'prepare'
 
-	function Templates(nested) {
-		this.defaultTemplate = null;
-		this.namedTemplates = {};
-		this.nested = nested != undefined ? nested : false;
+    function Templates(nested) {
+        this.defaultTemplate = null;
+        this.namedTemplates = {};
+        this.nested = nested != undefined ? nested: false;
 
-		return this;
-	}
+        return this;
+    }
 
-	Templates.prototype = {
-		parse : function(container) {
+    Templates.prototype = {
+        parse: function(container) {
 
-			var children = container.getElementsByTagName('*');
+            var children = container.getElementsByTagName('*');
 
-			for (var i = 0; i < children.length; i++) {
-				if ( children[i].getAttribute('data-template') != null && (this.nested || !utils.isNested(children[i]))) {
-					var element = children[i].cloneNode(true);
+            for (var i = 0; i < children.length; i++) {
+                if (children[i].getAttribute('data-template') != null && (this.nested || !utils.isNested(children[i]))) {
+                    var element = children[i].cloneNode(true);
 
-					// Remapping container element in case template is deep in container
-					this.container = children[i].parentNode;
+                    // Remapping container element in case template is deep in container
+                    this.container = children[i].parentNode;
 
-					// Element is a template
-					for (var a = 0; a < element.attributes.length; a++) {
-						var attr = element.attributes[a];
-						// If attribute
-						if (utils.startsWith(attr.name, 'data-if-')) {
-							var val;
-							if (attr.value == '') {
-								val = true;
-							} else {
-								val = '\'' + attr.value + '\'';
-							}
-							this.namedTemplates[attr.name.substring(8, attr.name.length) + '==' + val] = element;
-							element.removeAttribute(attr.name);
-						}
-					}
+                    // Element is a template
+                    for (var a = 0; a < element.attributes.length; a++) {
+                        var attr = element.attributes[a];
+                        // If attribute
+                        if (utils.startsWith(attr.name, 'data-if-')) {
+                            var val;
+                            if (attr.value == '') {
+                                val = true;
+                            } else {
+                                val = '\'' + attr.value + '\'';
+                            }
+                            this.namedTemplates[attr.name.substring(8, attr.name.length) + '==' + val] = element;
+                            element.removeAttribute(attr.name);
+                        }
+                    }
 
-					// Setting as default template, last one wins
-					this.defaultTemplate = element;
-				}
-			}
+                    // Setting as default template, last one wins
+                    this.defaultTemplate = element;
+                }
+            }
 
-			utils.clearContainer(this.container);
-		}
-	}
+            utils.clearContainer(this.container);
+        }
+    }
 
-	/*!
+    /*!
 	 * Renderer for populating containers with data using templates.
 	 */
-	function Renderer(templates) {
-		this.templates = templates;
-		this.container = templates.container;
+    function Renderer(templates) {
+        this.templates = templates;
+        this.container = templates.container;
 
-		return this;
-	}
+        return this;
+    }
 
-	Renderer.prototype = {
-		render : function(data) {
-			utils.clearContainer(this.container);
+    Renderer.prototype = {
+        render: function(data) {
+            utils.clearContainer(this.container);
+            if (data != null) {
+                for (var i = 0; i < data.length; i++) {
+                    var renderItem = function(renderer, item) {
+                        var template = renderer.templateFor(item);
+                        if (template != null) {
+                            renderer.template = template;
+                            renderer.item = item;
 
-			for (var i = 0; i < data.length; i++) {
-				var renderItem = function(renderer, item) {
-					var template = renderer.templateFor(item);
-					if (template != null) {
-						renderer.template = template;
-						renderer.item = item;
+                            var test = template.innerHTML.match(/data-template="(.*?)"/);
+                            if (test != null) {
+                                var t = new Templates(true);
+                                t.parse(renderer.template);
 
-						// Functions
-						for (var regex in renderer.functions) {
-							template.innerHTML = template.innerHTML.replace(new RegExp(regex, 'gi'), renderer.functions[regex](renderer));
-						}
+                                var r = new Renderer(t);
+                                r.render(renderer.item[test[1]]);
+                            }
 
-						// Content
-						template.innerHTML = utils.replaceVariable(item, template.innerHTML);
+                            // Functions
+                            for (var regex in renderer.functions) {
+                                template.innerHTML = template.innerHTML.replace(new RegExp(regex, 'gi'), renderer.functions[regex](renderer));
+                            }
 
-						// Template class attribute
-						if (template.getAttribute('class') != null) {
-							template.className = utils.replaceVariable(item, template.className);
-						}
+                            // Content
+                            template.innerHTML = utils.replaceVariable(item, template.innerHTML);
 
-						// Template id
-						if (template.getAttribute('id') != null) {
-							template.id = utils.replaceVariable(item, template.id);
-						}
+                            // Template class attribute
+                            if (template.getAttribute('class') != null) {
+                                template.className = utils.replaceVariable(item, template.className);
+                            }
 
-						renderer.container.appendChild(template);
-					}
-				}(this, data[i]);
-			}
+                            // Template id
+                            if (template.getAttribute('id') != null) {
+                                template.id = utils.replaceVariable(item, template.id);
+                            }
 
-			return this;
-		},
+                            renderer.container.appendChild(template);
+                        }
+                    } (this, data[i]);
+                }
+            }
 
-		templateFor : function(item) {
-			for (var templateName in this.templates.namedTemplates) {
-				if (eval('item.' + templateName)) {
-					return this.templates.namedTemplates[templateName].cloneNode(true);
-				}
-			}
-			if (this.templates.defaultTemplate != null) {
-				return this.templates.defaultTemplate.cloneNode(true);
-			}
-		},
+            return this;
+        },
 
-		functions : {
-			// If function
-			'{{if (.*?)}}(.*?){{endif}}' : function(renderer) {
-				return function(match, condition, content) {
-					var member_regex = '';
-					for (var member in renderer.item) {
-						if (member_regex.length > 0) {
-							member_regex += '|'
-						}
-						member_regex += member;
-					}
+        templateFor: function(item) {
+            for (var templateName in this.templates.namedTemplates) {
+                if (eval('item.' + templateName)) {
+                    return this.templates.namedTemplates[templateName].cloneNode(true);
+                }
+            }
+            if (this.templates.defaultTemplate != null) {
+                return this.templates.defaultTemplate.cloneNode(true);
+            }
+        },
 
-					condition = condition.replace(/&amp;/g, '&');
-					condition = condition.replace(new RegExp(member_regex, 'gi'), function(match) {
-						return 'renderer.item.' + match;
-					});
+        functions: {
+            // If function
+            '{{if (.*?)}}(.*?){{endif}}': function(renderer) {
+                return function(match, condition, content) {
+                    var member_regex = '';
+                    for (var member in renderer.item) {
+                        if (member_regex.length > 0) {
+                            member_regex += '|'
+                        }
+                        member_regex += member;
+                    }
 
-					if (eval(condition)) {
-						return content;
-					}
+                    condition = condition.replace(/&amp;/g, '&');
+                    condition = condition.replace(new RegExp(member_regex, 'gi'),
+                    function(match) {
+                        return 'renderer.item.' + match;
+                    });
 
-					return '';
-				}
-			},
+                    if (eval(condition)) {
+                        return content;
+                    }
 
-			// Nested templates
-
-			'<(.*?) data-template="(.*?)".*?>[\\s\\S]*?</\\1>' : function(renderer) {
-				return function(match, tag, member) {
-					if (renderer.item[member] != undefined) {
-						var t = new Templates(true);
-						t.parse(renderer.template);
-
-						var r = new Renderer(t);
-						r.render(renderer.item[member]);
-
-						return r.container.innerHTML;
-					}
-
-					return '';
-				}
-			}
-		}
-	}
-	Renderer.prototype.render.displayName = 'render';
+                    return '';
+                }
+            }
+        }
+    }
+    Renderer.prototype.render.displayName = 'render';
 
 
-	/*!
+    /*!
 	 * Helpers
 	 */
-	var utils = {
-		startsWith : function(str, prefix) {
-			return (str.indexOf(prefix) === 0);
-		},
+    var utils = {
+        startsWith: function(str, prefix) {
+            return (str.indexOf(prefix) === 0);
+        },
 
-		replaceVariable : function(item, str) {
-			return str.replace(/{{([A-Za-z0-9\.\_]*?)}}/g, replace = function(match, variable) {
-				var val = eval('item.' + variable)
-				if (val) {
-					return val;
-				}
-				return '';
-			});
-		},
+        replaceVariable: function(item, str) {
+            return str.replace(/{{([A-Za-z0-9\.\_]*?)}}/g, replace = function(match, variable) {
+                var val = eval('item.' + variable)
+                if (val) {
+                    return val;
+                }
+                return '';
+            });
+        },
 
-		clearContainer : function (el) {
-			if (el != null && el.childNodes != undefined) {
-				for (var i = el.childNodes.length; i >= 0; i--) {
-					if (el.childNodes[i] != null && el.childNodes[i].getAttribute != undefined && el.childNodes[i].getAttribute('data-template') != null) {
-						el.childNodes[i].parentNode.removeChild(el.childNodes[i]);
-					}
-				}
-			}
-		},
+        clearContainer: function(el) {
+            if (el != null && el.childNodes != undefined) {
+                for (var i = el.childNodes.length; i >= 0; i--) {
+                    if (el.childNodes[i] != null && el.childNodes[i].getAttribute != undefined && el.childNodes[i].getAttribute('data-template') != null) {
+                        el.childNodes[i].parentNode.removeChild(el.childNodes[i]);
+                    }
+                }
+            }
+        },
 
-		isNested : function(el) {
-			var p = el.parentNode;
-			while (p != null) {
-				if (p.getAttribute != undefined && p.getAttribute('data-template') != null) {
-					return true;
-				}
-				p = p.parentNode;
-			}
-			return false;
-		}
-	}
-	utils.startsWith.displayName = 'startsWith';
-	utils.replaceVariable.displayName = 'replaceVariable';
-	utils.clearContainer.displayName = 'clearContainer';
-	utils.isNested.displayName = 'isNested';
+        isNested: function(el) {
+            var p = el.parentNode;
+            while (p != null) {
+                if (p.getAttribute != undefined && p.getAttribute('data-template') != null) {
+                    return true;
+                }
+                p = p.parentNode;
+            }
+            return false;
+        }
+    }
+    utils.startsWith.displayName = 'startsWith';
+    utils.replaceVariable.displayName = 'replaceVariable';
+    utils.clearContainer.displayName = 'clearContainer';
+    utils.isNested.displayName = 'isNested';
 
-	return simplate;
+    return simplate;
 
 })(SIMPLATE || {});
