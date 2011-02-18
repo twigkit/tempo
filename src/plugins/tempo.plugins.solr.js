05d9297b56a68d0dff253bed8cadc2167fc149dc
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2011-02-18 17:05:41 +0000

    working on thoughts on plugins

--- src/plugins/tempo.plugins.solr.js
+++ src/plugins/tempo.plugins.solr.js
@@ -1,12 +1,4 @@
 Tempo.solr = (function (tempo) {
-    
-    var plugin = {
-        functions : {
-            '{{.*}}' : function() {
-
-            }
-        }
-    };
 
     return tempo;
     
