316c4fa1e92a975b5d7dc67116a4f63e555db1c7
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2011-07-21 21:54:59 +0100

    added comments

--- node/server.js
+++ node/server.js
@@ -1,19 +1,25 @@
 var http = require('http');
 var fs = require('fs');
 var sys = require('sys');
-var tempo = require('./module/tempo')
+var tempo = require('tempo')
 
+// Create an instance of an http server
 http.createServer(
         function(req, res) {
+			// Load some data
             var data = [
                 {'name': 'John'},
                 {'name': 'Paul'},
                 {'name': 'Ringo'}
             ];
 
+			// Load a template file and create an instance of Tempo
+			// Load method supplies an instance of Tempo to the callback function
             tempo.load('index.html', function(tempo) {
 
+				// Prepare a part of the template and render with data
                 tempo.prepare('names').render(data);
+				// Write the template to the response
                 tempo.write(res);
                 
                 res.end();
