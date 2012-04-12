74a93362e8ec2286a1a7b66d7475752aa14b0770
Hjortur Stefan Olafsson <mr.olafsson@gmail.com>
2012-04-12 12:51:09 +0100

    testing yql hack

--- examples/js/yql.js
+++ examples/js/yql.js
@@ -23,10 +23,10 @@ jQuery.ajax = (function(_ajax){
     }
 
     return function(o) {
-        console.log('bing')
         var url = o.url;
 
         if ( /get/i.test(o.type) && !/json/i.test(o.dataType) && isExternal(url) ) {
+            console.log('bong')
 
             // Manipulate options so that JSONP-x request is made to YQL
 
